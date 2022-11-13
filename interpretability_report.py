71c9b4bbc2d34df422c29289111dc9cce535847d
natan-dot-com <natanhsanches@gmail.com>
2022-11-13 20:11:56 -0300

    Add header function and text width support

--- interpretability_report.py
+++ interpretability_report.py
@@ -1,7 +1,8 @@
 import logging
 from reportlab.lib.enums import TA_JUSTIFY, TA_CENTER
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image, Table
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image, Table, TableStyle
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.pagesizes import A4
 from reportlab.lib.units import inch
 from itertools import zip_longest
 from os.path import join, basename, exists
@@ -14,16 +15,42 @@ report_handler.setFormatter(logging.Formatter("%(name)s - %(levelname)s - %(mess
 report_logger = logging.getLogger(basename(__file__))
 report_logger.addHandler(report_handler)
 
-REPORT_MAIN_TITLE = "Model Interpretability Report (BioAutoML)"
-REPORT_SHAP_PREAMBLE = "Método Shap: SHAP calcula para cada amostra qual a importância de cada  feature para a decisão de classificação."
-REPORT_SHAP_SUMMARY = "Lorem ipsum"
-REPORT_SHAP_WATERFALL = "Lorem ipsum"
+REPORT_MAIN_TITLE_MULTICLASS = "Model Interpretability Report (Multiclass)"
+REPORT_SHAP_PREAMBLE = (
+	"All of interpretability are based in the SHAP method, in which it calculates what's the importance "
+	"level for each feature in the classification process. It uses shapley values, a game theory concept, "
+	"as a descriptive metric to create an hierarquical structure between the features."
+)
+REPORT_SUMMARY_TITLE = "Summary Plots"
+REPORT_SHAP_SUMMARY_1 = (
+	"The above plot is called Summary plot and it shows, for each class, how low/high values of each feature "
+	"contributed for the classification with that class. The features are ranked from most descriptive to "
+	"least descriptive. This plot is a summarization of all the entries in the test set."
+)
+REPORT_SHAP_SUMMARY_2 = (
+	"An impact with positive SHAP value means that a high (red dots), medium (purple dots) or low (blue dots) "
+	"feature value contributes positively of an entry to be classified with that class. The inverse happens with " 
+	"negative SHAP values." 
+)
+REPORT_WATERFALL_TITLE = "Waterfall Plots"
+REPORT_SHAP_WATERFALL = lambda n_samples: (
+	"A Waterfall plot shows, for some entries, which of the features contributed more for it to be classified with "
+	f"its classified class. In this case, {n_samples} samples for each class were chosen randomly to be analyzed."
+)
+
+make_bold = lambda s: f"<b>{s}</b>"
+make_font_size = lambda s, size: f"<font size={size}>{s}</font>"
 
 class Report:
-	def __init__(self, report_name, directory=".", lr_margin=12, tb_margin=12):
-		"""
-			Create a new PDF report with filename 'report_name'
-		"""
+	styles = None
+	story = None
+	doc = None
+	text_width = None
+
+	def __init__(self, report_name, directory=".", lr_margin=float(0.5*inch), tb_margin=float(0.25*inch)):
+		
+		"""Create a new PDF report with filename 'report_name'"""
+		
 		self.styles = getSampleStyleSheet()
 		self.story = []
 		self.doc = SimpleDocTemplate(
@@ -31,10 +58,16 @@ class Report:
 			leftMargin=lr_margin,
 			rightMargin=lr_margin,
 			topMargin=tb_margin,
-			bottomMargin=tb_margin
+			bottomMargin=tb_margin,
+			pagesize=A4
 		)
-		self.styles.add(ParagraphStyle(name='Justify', fontName="Courier",alignment=TA_JUSTIFY))
-		self.styles.add(ParagraphStyle(name='Center', fontName="Courier",alignment=TA_CENTER))
+		self.styles.add(ParagraphStyle(name='Justify', fontName="Helvetica", 
+						alignment=TA_JUSTIFY, firstLineIndent=0.3*inch))
+		self.styles.add(ParagraphStyle(name='Center', fontName="Helvetica", 
+						alignment=TA_CENTER))
+		
+		page_width, _ = A4
+		self.text_width = page_width - 2*lr_margin
 		
 
 	def create_paragraph_style(self, style_name, font_name, alignment):
@@ -46,7 +79,36 @@ class Report:
 		)
 
 
-	def insert_text_on_doc(self, text, font_size=12, style='Justify', pre_margin=1, pos_margin=12):
+	def insert_doc_header(self, title, font_size=16, logo_fig=None, pre_margin=1, pos_margin=18, bold=True):
+		if not logo_fig:
+			self.insert_text_on_doc(title, font_size=font_size, style='Center', pos_margin=pos_margin, bold=bold)
+			return
+
+		if pre_margin > 0:
+			self.story.append(Spacer(1, pre_margin))
+		else:
+			report_logger.warning(f"'pre_margin' can't be negative. Ignoring it " +\
+							  	  f"and using default value (1). [pre_margin={pre_margin}]")
+
+		assert exists(logo_fig), f"Logo figure in path {logo_fig} does not exist."
+		fmt = make_font_size(make_bold(title) if bold else title, font_size)
+		self.story.append(Table(
+			[
+				[Paragraph(fmt, self.styles['Center']), 
+			  	Image(logo_fig, width=0.15*self.text_width, height=10*inch, kind='proportional')]
+			],
+			style=TableStyle([('VALIGN', (0,0), (1,0), 'MIDDLE')]), 
+			colWidths=[0.8*self.text_width, 0.2*self.text_width]
+		))
+
+		if pos_margin > 0:
+			self.story.append(Spacer(1, pos_margin))
+		else:
+			report_logger.warning(f"'pos_margin' can't be negative. Ignoring it " +\
+							 	  f"and using default value (18). [pos_margin={pos_margin}]")
+
+
+	def insert_text_on_doc(self, text, font_size=12, style='Justify', pre_margin=1, pos_margin=12, bold=False):
 		
 		"""Insert a new paragraph on report with given text customization"""
 
@@ -54,20 +116,20 @@ class Report:
 			self.story.append(Spacer(1, pre_margin))
 		else:
 			report_logger.warning(f"'pre_margin' can't be negative. Ignoring it " +\
-							  f"and using default value (1). [pre_margin={pre_margin}]")
+							  	  f"and using default value (1). [pre_margin={pre_margin}]")
 
 		assert font_size > 0, f"Error: 'font_size' can't be negative. Aborted. [font_size={font_size}]"
-		fmt = f"<font size={font_size}>{text}</font>"
+		fmt = make_font_size(make_bold(text) if bold else text, font_size)
 		self.story.append(Paragraph(fmt, self.styles[style]))
 
 		if pos_margin > 0:
 			self.story.append(Spacer(1, pos_margin))
 		else:
 			report_logger.warning(f"'pos_margin' can't be negative. Ignoring it " +\
-							  f"and using default value (12). [pos_margin={pos_margin}]")
+							 	  f"and using default value (12). [pos_margin={pos_margin}]")
 
 
-	def insert_figure_on_doc(self, fig_paths, image_width=4*inch, image_height=3*inch, pre_margin=1, pos_margin=24):
+	def insert_figure_on_doc(self, fig_paths, pre_margin=1, pos_margin=24):
 		"""
 			Insert a list of figures pairwise into the report
 			If the size of the list is odd, the last one will be centered
@@ -83,16 +145,13 @@ class Report:
 				report_logger.warning(f"'pre_margin' can't be negative. Ignoring it " +\
 									  f"and using default value (1). [pre_margin={pre_margin}]")
 
-			assert image_width > 0, f"Error: 'image_width' can't be negative. Aborted. image_width=[{image_width}]"
-			assert image_height > 0, f"Error: 'image_height' can't be negative. Aborted. image_height=[{image_height}]"
-
 			if not fig2:
-				self.story.append(Image(fig, width=image_width, height=image_height, kind="proportional"))
+				self.story.append(Image(fig, width=0.5*self.text_width, height=10*inch, kind="proportional"))
 			else:
 				assert exists(fig2), f"Figure in path {fig2} does not exist."
 				self.story.append(Table(
-					[[Image(fig, width=image_width, height=image_height, kind="proportional"),\
-					 Image(fig2, width=image_width, height=image_height, kind="proportional")]]
+					[[Image(fig, width=0.5*self.text_width, height=10*inch, kind="proportional"),\
+					  Image(fig2, width=0.5*self.text_width, height=10*inch, kind="proportional")]]
 				))
 
 			if pos_margin > 0:
