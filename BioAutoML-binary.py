e9647752fa7160b9e3dc7615e025d43d7839975b
0nurB <78937509+0nurB@users.noreply.github.com>
2022-11-23 18:11:35 -0300

    Update BioAutoML-binary.py

--- BioAutoML-binary.py
+++ BioAutoML-binary.py
@@ -55,8 +55,8 @@ from reportlab.lib.enums import TA_JUSTIFY, TA_CENTER #add by Bruno
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image #add by Bruno
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle #add by Bruno
 from numpy.random import default_rng #add by Bruno
-from interpretability_report import Report, REPORT_MAIN_TITLE, REPORT_SHAP_PREAMBLE, REPORT_SHAP_BAR, \
-	REPORT_SHAP_BEESWARM, REPORT_SHAP_WATERFALL
+from interpretability_report import Report, REPORT_MAIN_TITLE_BINARY, REPORT_SHAP_PREAMBLE_BINARY, REPORT_SHAP_BAR_BINARY, \
+	REPORT_SHAP_BEESWARM_BINARY, REPORT_SHAP_WATERFALL_BINARY
 
 def header(output_header):
 
